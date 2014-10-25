c83ea1d0bf2b7f0bf9117358b7ef8fbc586c9881
joe <joeramsfan@hotmail.co.uk>
2014-10-25 14:42:17 +0100

    Added stuff to player

--- src/poker/Player.java
+++ src/poker/Player.java
@@ -2,7 +2,24 @@ package poker;
 
 public class Player {
 	
+	private String name;
 	private int chips;
 	
+	public Player(String name, int startingAmmount){
+		this.chips = startingAmmount;
+		this.name = name;
+	}
+	
+	public void increaseChips(int ammount){
+		chips+=ammount;
+	}
+	
+	public void decreaseChips(int ammount){
+		chips-=ammount;
+	}
+	
+	private void setChips(int c){
+		chips = c;
+	}
 	
 }
