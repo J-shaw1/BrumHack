2b23e28cd846ee16bb89682100369fd48314fd37
kinleyradams <kinleyradams@gmail.com>
2014-10-25 17:47:35 +0100

    fgfgdf

---
+++ src/poker/PokerRound.java
@@ -0,0 +1,65 @@
+package poker;
+
+import java.util.ArrayList;
+
+public class PokerRound {
+	
+	private static final int LOOP_TWO_TIMES = 2;
+	private static final int MAX_AMOUNT_AI = 4;
+	
+	private ArrayList<Player> players;
+	private Deck deck = new Deck();
+	private int roundNumber;
+	private Pot pot = new Pot(players.size());
+	
+	public PokerRound(ArrayList<Player> players, int roundNumber){
+		this.players = players;
+		this.roundNumber = roundNumber;
+	}
+
+	public ArrayList<Player> start(int sizeOfBBlind) {
+		
+		int x = 0;
+		// Assign Blind
+		if (roundNumber <= players.size()){
+			players.get(roundNumber).setBlind(Blind.Small);
+			if (roundNumber + 1 <= players.size()){
+				players.get(roundNumber + 1).setBlind(Blind.Big);
+			} else {
+				x = roundNumber % players.size();
+				players.get(x).setBlind(Blind.Big);
+			}
+		} else {
+			x = roundNumber % players.size();
+			players.get(x).setBlind(Blind.Small);
+			players.get(x + 1).setBlind(Blind.Big);
+		}
+		
+		//set some vars to make it easier later
+		int iSB = roundNumber % players.size();
+		
+		
+		//Give 2 cards each
+		for (int j = 0; j < LOOP_TWO_TIMES; j++) {
+			for (int i = 0; i < MAX_AMOUNT_AI + 1; i++) {
+				(players.get(i)).addCardToHand(deck.getCard());
+			}
+		}
+		
+		//place blinds
+		
+		//small blind + big blind
+		for (int i = 0; i < MAX_AMOUNT_AI + 1; i++) {
+			if( (players.get(i)).getBlind() == Blind.Small){
+				pot.bet(sizeOfBBlind / 2, roundNumber, i);
+				pot.bet(sizeOfBBlind, roundNumber, i + 1);
+			}
+		}
+		
+		
+		
+		
+		return players;
+	}//end of start()
+
+}
