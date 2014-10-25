23a6dad6a719d40f670cb27a42596b749745f058
joe <joeramsfan@hotmail.co.uk>
2014-10-25 18:01:58 +0100

    Created array list in hand

--- src/poker/Hand.java
+++ src/poker/Hand.java
@@ -3,23 +3,26 @@ package poker;
 import java.util.ArrayList;
 
 public class Hand {
-	
+
 	private ArrayList<Card> cards;
-	
+
 	public Hand(){
+		cards = new ArrayList<>();
 	}
-	
-	public void addCard(Card c) throws Exception{
+
+	public void addCard(Card c) throws Exception {
 		cards.add(c);
-		if(cards.size() > 2) throw new Exception("Too many cards in the hand") ;
+		if (cards.size() > 2)
+			throw new Exception("Too many cards in the hand");
 	}
-	
-	public Card[] getHand(){
+
+	public Card[] getHand() {
 		return (Card[]) cards.toArray();
 	}
-	
+
 	public String toString() {
-		return "Card 1: " + cards.get(0).toString() + " Card 2:" + cards.get(1).toString();
+		return "Card 1: " + cards.get(0).toString() + " Card 2:"
+				+ cards.get(1).toString();
 	}
-	
+
 }
