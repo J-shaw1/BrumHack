395562fb84c923bf630964a0d2ae930a7b94283a
joe <joeramsfan@hotmail.co.uk>
2014-10-26 02:38:12 +0000

    calc odds

---
+++ src/poker/CalculateOdds.java
@@ -0,0 +1,13 @@
+package poker;
+
+import java.util.ArrayList;
+
+public class CalculateOdds {
+	public static float calculateOdds(Hand hand, ArrayList<Card> cards){
+		 //Need to work out how many cards would help me
+		
+		
+		
+		return 0;
+	}
+}
